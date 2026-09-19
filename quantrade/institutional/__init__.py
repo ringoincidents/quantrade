913384cf7d97c6979296a7fcd94f883e7140dc27
ringoincidents <86282567+ringoincidents@users.noreply.github.com>
2026-09-19 18:17:34 +0900

    Add quantrade/institutional/__init__.py

---
+++ quantrade/institutional/__init__.py
@@ -0,0 +1,5 @@
+"""QuanTrade institutional runtime kernel."""
+
+from .service import InstitutionalKernel, InvalidTransition, ImmutableRecordError
+
+__all__ = ["InstitutionalKernel", "InvalidTransition", "ImmutableRecordError"]
